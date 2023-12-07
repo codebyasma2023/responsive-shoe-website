2bab9a1a40ac9b298bf38c48b82a47b8217bffa8
Asma <147394980+codebyasma2023@users.noreply.github.com>
2023-12-07 04:41:38 -0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,36 @@
+/*=============== SHOW MENU ===============*/
+
+
+/*====== MENU SHOW ======*/
+
+
+/*====== MENU HIDDEN ======*/
+
+
+
+/*=============== REMOVE MENU MOBILE ===============*/
+
+
+
+/*=============== SHADOW HEADER ===============*/
+
+
+
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+
+
+
+/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+
+ 
+/*=============== SHOW SCROLL UP ===============*/ 
+
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+
+
+/*=============== DARK LIGHT THEME ===============*/ 
+
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+
